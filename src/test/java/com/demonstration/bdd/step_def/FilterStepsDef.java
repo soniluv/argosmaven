6154e31260e1fe8ee30dabbd631c9e36d5004614
K S <sonieuphoria@googlemail.com>
2018-06-01 15:38:29 +0100

    Login scenarios

--- src/test/java/com/demonstration/bdd/step_def/FilterStepsDef.java
+++ src/test/java/com/demonstration/bdd/step_def/FilterStepsDef.java
@@ -17,6 +17,21 @@ public class FilterStepsDef {
     HeaderPage headerPage = new HeaderPage();
     ResultsPage resultsPage = new ResultsPage();
 
+
+    @When("^I select a product$")
+    public void i_select_a_product() throws Throwable {
+
+    }
+
+    @When("^I add product to basket$")
+    public void i_add_product_to_basket() throws Throwable {
+
+    }
+
+    @Then("^I should see product in basket$")
+    public void i_should_see_product_in_basket() throws Throwable {
+
+    }
     @Given("^when I am on the home page$")
     public void when_I_am_on_the_home_page() throws Throwable {
 
@@ -37,6 +52,32 @@ public class FilterStepsDef {
     public void i_should_see_results_filtered_by(Double expectedValue) throws Throwable {
         List<Double> actualList= resultsPage.getAllReviewRatings();
         assertThat(actualList, everyItem(greaterThan(expectedValue)));
+    }
+
+
+    @Given("^I have registered for argos$")
+    public void i_have_registered_for_argos() throws Throwable {
+
+    }
+
+    @Given("^I am on the home page$")
+    public void i_am_on_the_home_page() throws Throwable {
+
+    }
+
+    @When("^I input my username and password$")
+    public void i_input_my_username_and_password() throws Throwable {
+
+    }
+
+    @When("^I click on submit button$")
+    public void i_click_on_submit_button() throws Throwable {
+
+    }
+
+    @Then("^I should see welcome message with my name$")
+    public void i_should_see_welcome_message_with_my_name() throws Throwable {
+
     }
 
 }
--- src/test/resources/Basket.feature
+++ src/test/resources/Basket.feature
@@ -1,5 +1,5 @@
 Feature:
-  Scenario: Add a product to basket
+  Scenario : Add a product to basket
     Given when I am on the home page
     When I search for a product "nike"
     And I select a product
